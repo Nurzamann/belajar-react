7af8e9d4731430b46b1f069893080b9f9d858fdd
Nurzamann <nurzaman0001@gmail.com>
2025-06-05 00:36:57 +0700

    menambahkan link di home page

--- react-basics-voidfnc/src/pages/HomePages.tsx
+++ react-basics-voidfnc/src/pages/HomePages.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router";
+
 const HomePages = () => {
-    return (
-        <div>
-            <h1>Ini Home Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Ini Home Page</h1>
+      <p>Setujui syarat dibawah ini</p>
+      <Link to="/terms">Menuju Halaman Terms</Link>
+      {/* Hanya ganti di browser, gak request ke server lagi */}
+    </div>
+  );
+};
 
 export default HomePages;
